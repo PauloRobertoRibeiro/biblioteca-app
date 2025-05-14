3f43fb164476032ad0f78ccebc0e54499dc5e36c
PauloRobertoRibeiro <ribeirorobertopaulo@gmail.com>
2025-05-14 21:22:07 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,22 @@
+const cacheName = 'biblioteca-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('biblioteca-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png',
-        './seu-arquivo-css.css',
-        './seu-arquivo-js.js'
-        // adicione outros arquivos necessários
-      ]);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
